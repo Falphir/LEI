badc0e712256056d3e9ebdac6b18981f768d0a97
Falphir <73762798+Falphir@users.noreply.github.com>
2023-10-03 17:32:05 +0100

    Resolução Aula 03/10/2023

--- SO_ficha1/src/main/java/net/tiago/so_ficha1/ex3/ex3.java
+++ SO_ficha1/src/main/java/net/tiago/so_ficha1/ex3/ex3.java
@@ -2,6 +2,7 @@
 package net.tiago.so_ficha1.ex3;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.*;
 import java.util.Scanner;
 
@@ -15,27 +16,32 @@ public class ex3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        File value_file = new File(args[0]);
-        
-        Scanner reader = new Scanner(value_file);
-        
-        int soma = 0;
-        int subtracao = 0;
-        int multiplicacao = 0;
-        int divisao = 0;
-        
-        while(reader.hasNextLine()) {
-            soma += reader.nextInt();
-            subtracao -= reader.nextInt();
-            multiplicacao *= reader.nextInt();
-            divisao /= reader.nextInt();
+
+        try{
+            File f = new File(args[0]);
+            Scanner scanner = new Scanner(f);
+            float soma = 0, subtracao = 0, multiplicacao = 1, divisao = 1;
+
+            if (scanner.hasNext()) {
+                int val1 = scanner.nextInt();
+                soma = val1;
+                subtracao = val1;
+                multiplicacao = val1;
+                divisao = val1;
+            }
+            while (scanner.hasNext()){
+                    int val = scanner.nextInt();
+                    soma += val;
+                    subtracao -= val;
+                    multiplicacao *= val;
+                    divisao /= val;
+            }
+            System.out.println("Soma: " + soma + "\n" + "Subtracao: " + subtracao + "\n" + "Multiplicacao: " + multiplicacao + "\n" + "Divisao: " + divisao);
+
+        }catch(Exception err){
+            err.printStackTrace();
         }
         
-        System.out.println("A soma dos dois valores e:" + soma);
-        System.out.println("A subtracao dos dois valores e: " + subtracao);
-        System.out.println("A multiplicacao dos dois valores e: " + multiplicacao);
-        System.out.println("A divisao dos dois valores e: " + divisao);
     }
     
 }

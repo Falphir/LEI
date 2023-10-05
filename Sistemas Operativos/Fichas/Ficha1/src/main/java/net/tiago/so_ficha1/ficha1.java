00fc6e3bee1d80ec2ec9da3ff876250fc8aab0f1
Falphir <73762798+Falphir@users.noreply.github.com>
2023-10-05 14:34:14 +0100

    Organização

--- Sistemas Operativos/Fichas/Ficha1/src/main/java/net/tiago/so_ficha1/ficha1.java
+++ Sistemas Operativos/Fichas/Ficha1/src/main/java/net/tiago/so_ficha1/ficha1.java
@@ -4,7 +4,7 @@ package net.tiago.so_ficha1;
  *
  * @author tiago
  */
-public class SO_ficha1 {
+public class ficha1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
